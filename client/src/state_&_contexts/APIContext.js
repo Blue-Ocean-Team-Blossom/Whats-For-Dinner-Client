018c4e1596c30b34b619095a68b1e613ec9e66ae
Daniel <d_suh95@yahoo.com>
2021-05-22 16:16:18 -0500

    Added getPantry with sample entry

--- client/src/state_&_contexts/APIContext.js
+++ client/src/state_&_contexts/APIContext.js
@@ -8,13 +8,30 @@ export const APIContext = createContext({});
 
 const APIProvider = ({ children }) => {
 
+  const { setPantry } = useContext(PantryContext);
+
   /*********************************FUNCTION CALLS GO HERE************************************/
+
+
+  /****************************************Pantry*********************************************/
+  const getPantry = () => {
+    var sampleItem = {
+      id: 1,
+      userId: 1,
+      ingredientId: 1
+      ingredient: 'something',
+      quantity: 1
+    }
+    return sampleItem;
+  }
 
   /*******************************************************************************************/
 
   return (
     <APIContext.Provider value={{
       /* Include functions here */
+      //Pantry
+      getPantry
     }}>
       {children}
     </APIContext.Provider>
