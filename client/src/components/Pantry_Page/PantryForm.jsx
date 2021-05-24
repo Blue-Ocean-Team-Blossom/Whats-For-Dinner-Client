1e5251d0364a96f7c3a23ceb6a9b28d1714f3a8d
Ian Ferrier <ian.m.ferrier@gmail.com>
2021-05-24 15:51:09 -0700

    Positioning for PantryForm horizontal now with labels above input fields.

--- client/src/components/Pantry_Page/PantryForm.jsx
+++ client/src/components/Pantry_Page/PantryForm.jsx
@@ -3,27 +3,24 @@ import { APIContext } from '../../state_&_contexts/APIContext';
 
 const PantryForm = () => {
   const { addToPantry } = useContext(APIContext);
-  const {
-    pantryForm, item, unit, quantity, submit,
-  } = styles;
   return (
-    <form className={pantryForm} onSubmit={(e) => addToPantry(e)}>
-      <label className={item} htmlFor="item">
+    <form id="pantryForm" onSubmit={(e) => addToPantry(e)}>
+      <label htmlFor="pantryFormItem">
         Item to add to pantry:
         <br />
-        <input type="text" id="item" name="item " placeholder="item to add" />
+        <input type="text" id="pantryFormItem" name="pantryFormItem" placeholder="item to add" />
       </label>
-      <label className={unit} htmlFor="unit">
+      <label htmlFor="pantryFormUnit">
         Unit measurement for item:
         <br />
-        <input type="text" id="unit" name="unit" placeholder="unit" disabled />
+        <input type="text" id="pantryFormUnit" name="pantryFormUnit" placeholder="unit" disabled />
       </label>
-      <label className={quantity} htmlFor="quantity">
+      <label htmlFor="pantryFormQuantity">
         Quantity for item:
         <br />
-        <input type="number" id="quantity" name="quantity" placeholder="greater than zero" step="0.01" min="0.01" />
+        <input type="number" id="pantryFormQuantity" name="pantryFormQuantity" placeholder="greater than zero" step="0.01" min="0.01" />
       </label>
-      <button className={submit} type="submit" id="submit">Submit</button>
+      <button type="pantryFormSubmit" id="pantryFormSubmit">Submit</button>
     </form>
   );
 };
