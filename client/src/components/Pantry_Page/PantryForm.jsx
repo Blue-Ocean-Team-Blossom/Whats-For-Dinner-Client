e7ea157ee8b7ed4d081560dee763199c12de6114
Ian Ferrier <ian.m.ferrier@gmail.com>
2021-05-24 15:11:51 -0700

    Issues still, but getting CSS modules setup.

--- client/src/components/Pantry_Page/PantryForm.jsx
+++ client/src/components/Pantry_Page/PantryForm.jsx
@@ -1,34 +1,28 @@
 import React, { useEffect, useContext, useState } from 'react';
 import { APIContext } from '../../state_&_contexts/APIContext';
+import styles from './pantryForm.module.css';
 
 const PantryForm = () => {
   const { addToPantry } = useContext(APIContext);
+  const { pantryForm, item, unit, quantity, submit } = styles;
   return (
-    <form onSubmit={(e) => addToPantry(e)}>
-      <div>
-        <label htmlFor="item">
-          Item to add to pantry:
-          <br />
-          <input type="text" id="item" name="item " placeholder="item to add" />
-        </label>
-      </div>
-      <div>
-        <label htmlFor="unit">
-          Unit measurement for item:
-          <br />
-          <input type="text" id="unit" name="unit" placeholder="unit" disabled />
-        </label>
-      </div>
-      <div>
-        <label htmlFor="quantity">
-          Quantity for item:
-          <br />
-          <input type="number" id="quantity" name="quantity" placeholder="greater than zero" step="0.01" min="0.01" />
-        </label>
-      </div>
-      <div>
-        <button type="submit">Submit</button>
-      </div>
+    <form className={pantryForm} onSubmit={(e) => addToPantry(e)}>
+      <label className={} htmlFor="item">
+        Item to add to pantry:
+        <br />
+        <input type="text" id="item" name="item " placeholder="item to add" />
+      </label>
+      <label className={} htmlFor="unit">
+        Unit measurement for item:
+        <br />
+        <input type="text" id="unit" name="unit" placeholder="unit" disabled />
+      </label>
+      <label className={} htmlFor="quantity">
+        Quantity for item:
+        <br />
+        <input type="number" id="quantity" name="quantity" placeholder="greater than zero" step="0.01" min="0.01" />
+      </label>
+      <button className={} type="submit" id="submit">Submit</button>
     </form>
   );
 };
