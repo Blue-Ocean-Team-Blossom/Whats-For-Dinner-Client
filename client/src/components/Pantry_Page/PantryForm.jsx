4c791557451f0db9576ccecf97238c957e911642
Ian Ferrier <ian.m.ferrier@gmail.com>
2021-05-24 15:19:32 -0700

    PantryForm styles in each tag now. Unsure if working.

--- client/src/components/Pantry_Page/PantryForm.jsx
+++ client/src/components/Pantry_Page/PantryForm.jsx
@@ -4,25 +4,27 @@ import styles from './pantryForm.module.css';
 
 const PantryForm = () => {
   const { addToPantry } = useContext(APIContext);
-  const { pantryForm, item, unit, quantity, submit } = styles;
+  const {
+    pantryForm, item, unit, quantity, submit,
+  } = styles;
   return (
     <form className={pantryForm} onSubmit={(e) => addToPantry(e)}>
-      <label className={} htmlFor="item">
+      <label className={item} htmlFor="item">
         Item to add to pantry:
         <br />
         <input type="text" id="item" name="item " placeholder="item to add" />
       </label>
-      <label className={} htmlFor="unit">
+      <label className={unit} htmlFor="unit">
         Unit measurement for item:
         <br />
         <input type="text" id="unit" name="unit" placeholder="unit" disabled />
       </label>
-      <label className={} htmlFor="quantity">
+      <label className={quantity} htmlFor="quantity">
         Quantity for item:
         <br />
         <input type="number" id="quantity" name="quantity" placeholder="greater than zero" step="0.01" min="0.01" />
       </label>
-      <button className={} type="submit" id="submit">Submit</button>
+      <button className={submit} type="submit" id="submit">Submit</button>
     </form>
   );
 };
