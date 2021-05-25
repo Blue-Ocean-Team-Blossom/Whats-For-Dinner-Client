b4f7c741d598efed48b7fba9be886d36f99b3066
Jason Tseng <jsntseng14@gmail.com>
2021-05-25 15:41:58 -0700

    styling for filtered section

--- client/src/components/Home_Page/components/FilteredRecipes.jsx
+++ client/src/components/Home_Page/components/FilteredRecipes.jsx
@@ -74,13 +74,19 @@ class FilteredRecipe extends React.Component {
 
   render() {
     return (
-      <div className ='search-bar'>
-        <SearchBar change={this.handleSearchChange}/>
-        <IngredientsList list={this.state.ingredients} add={this.addIngredient}/>
-        <div className="filter">FILTER</div>
-        <FilteredList list={this.state.filter} delete={this.deleteFilter}/>
-        <button type="button" onClick={this.handleSearchButton}>Search</button>
-        <Recipe recipeList={this.state.recipes}/>
+      <div className="filteredContainer">
+        <div className ='search-bar'>
+          <SearchBar change={this.handleSearchChange}/>
+          <IngredientsList list={this.state.ingredients} add={this.addIngredient}/>
+        </div>
+        <div className="filter-list">
+          <div className="filter">FILTER</div>
+          <button className="btn" type="button" onClick={this.handleSearchButton}>Search</button>
+          <FilteredList list={this.state.filter} delete={this.deleteFilter}/>
+        </div>
+        <div>
+          <Recipe className="filteredRecipes" recipeList={this.state.recipes}/>
+        </div>
       </div>
     )
   }
