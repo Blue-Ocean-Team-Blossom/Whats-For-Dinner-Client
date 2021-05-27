f96566aac356093592edf6ace3929dc94e424be4
Jheuton7 <jeffreyheuton1@gmail.com>
2021-05-27 12:05:46 -0600

    login loads pantry

--- client/src/components/Login_Signup_Page/Login_Signup.jsx
+++ client/src/components/Login_Signup_Page/Login_Signup.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useContext, useState } from 'react';
 import { APIContext } from '../../state_&_contexts/APIContext';
 
-const Login_Signup = () => {
+const Login_Signup = (props) => {
   const { createUser, userLogin } = useContext(APIContext);
 
   const [onLoginPage, setOnLoginPage] = useState(true);
@@ -16,11 +16,11 @@ const Login_Signup = () => {
     <div className='login'>
       {onLoginPage
        ? <div className='loginBox'>
-          <button className='btn fill' type="button" onClick={() => switchPage()}>
+          <button className='btn fill' type="button" onClick={() => {switchPage()}}>
             New User? Sign Up Here!
           </button>
           <h4>Login</h4>
-          <form onSubmit={(e) => userLogin(e)}>
+          <form onSubmit={(e) => {userLogin(e); setTimeout(() => {props.changeView('pantryList')}, 500)}}>
             <label className='loginItem' htmlFor="username">
               <input className='input' type='text' id="username" name="username" placeholder='username' required />
             </label>
@@ -35,7 +35,7 @@ const Login_Signup = () => {
             Have an account? Log In Here!
           </button>
           <h4>Sign Up</h4>
-          <form onSubmit={(e) => createUser(e)}>
+          <form onSubmit={(e) => {createUser(e); setTimeout(() => {props.changeView('pantryList')}, 500)}}>
             <label className='loginItem' htmlFor="username">
               <input className='input' type='text' id="username" name="username" placeholder='username' required />
             </label>
